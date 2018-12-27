c82c922526f31a7ff01ff466ee3fa4851f39af0a
Pavel Ivashkov <pavels.code@gmail.com>
2018-12-27 13:36:15 +0200

    day 19

--- code/19-1-go-with-the-flow/solve.cpp
+++ code/19-1-go-with-the-flow/solve.cpp
@@ -7,7 +7,7 @@ typedef uint32_t u32;
 
 
 int main(int argc, char const *argv[]) {
-    u32 r0 = 1, r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0;
+    u32 r0 = 0, r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0;
 
     r2 += 2;
     r2 *= r2;
@@ -30,16 +30,14 @@ int main(int argc, char const *argv[]) {
 
     r4 = 1;
     do {
-        // r1 = 1;
-        r1 = r2 / r4;
+        r1 = 1;
         do {
             r5 = r4 * r1;
             if (r5 == r2) {
                 r0 += r4;
             }
-        //     r1 += 1;
-        // } while (r1 <= r2);
-        } while (false);
+            r1 += 1;
+        } while (r1 <= r2);
         r4 += 1;
     } while (r4 <= r2);
 
